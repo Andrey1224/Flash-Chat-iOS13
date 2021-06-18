8b11300bceb664a448af7aaf0161fdc303e20224
Andrey1224 <nepod77@gmail.com>
2021-06-17 20:47:43 -0400

    Registering New users , logging in existing users , logging out users.

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,37 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
+    
+    let alert = UIAlertController(title: "Hey!", message: "The password must be 6 characters long or more.", preferredStyle: UIAlertController.Style.alert)
+    
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if  let email = emailTextfield.text, let password = passwordTextfield.text {
+            
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                
+                if let e = error {
+                    print(e.localizedDescription)
+                    self.alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(self.alert, animated: true, completion: nil)
+                    
+                } else {
+                    
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    
+                }
+                
+            }
+            
+        }
+        
     }
     
 }
